f6c9b157bf671c8f1ad61e02d32e06d482353683
ketankumar <ketanicengg@yahoo.com>
2019-07-23 15:18:21 +0100

    initial commit

---
+++ src/main/java/gridseleniummaven/App.java
@@ -0,0 +1,13 @@
+package gridseleniummaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
